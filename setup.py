b67d25bb7dff9d1e07ad70a0214fb426c1019b87
Piotr Rarus <piotr.rarus@softwarehut.com>
2019-12-02 13:58:08 +0100

    Updated version number

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
 
     url='https://github.com/piotr-rarus/degas',
     license='MIT',
-    version='0.1.3',
+    version='0.1.4',
 
     long_description=long_description,
     long_description_content_type='text/markdown',
